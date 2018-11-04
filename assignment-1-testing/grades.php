4c0e874efefed2b8c71d5e8c56c9202f41114c9b
AsmaaSobhyy <36643080+AsmaaSobhyy@users.noreply.github.com>
2018-11-04 17:44:17 +0200

    main outline html

---
+++ assignment-1-testing/grades.php
@@ -0,0 +1,76 @@
+<?php include("header.php"); ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>students</title>
+    
+</head>
+<body>
+<div class="container">
+<div  class="p-2 mt-3 text-align-center text-justify">
+		<span style="font-size: 150%;" class="text-monospace" >Grades</span>
+
+	</div>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">name</th>
+      <th scope="col">subject1</th>
+      <th scope="col">subject 2</th>
+      <th scope="col">subject 3</th>
+      <th scope="col"> </th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    	<td>Ahmed Mohamed</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>Ahmed Mohamed</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>Ahmed Mohamed</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>Ahmed Mohamed</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>50</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<footer class="footer">
+<?php include("footer.php"); ?>
+</footer>
+    
+</body>
+</html>
