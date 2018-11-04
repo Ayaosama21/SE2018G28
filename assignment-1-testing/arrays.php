67b30469859593d9ff127ef717414c5751f12da0
AsmaaSobhyy <36643080+AsmaaSobhyy@users.noreply.github.com>
2018-11-04 19:45:31 +0200

    test data arrays

---
+++ assignment-1-testing/arrays.php
@@ -0,0 +1,6 @@
+<?php
+$students =[ 1 => 'ahmed', 2=>'mohamed', 3=>'ahmed',4=>'ahmed', 5=>'ahmed'];
+$cources = [1 => 'math' ,2 => 'physics' ,3 => 'CO' ];
+$courcesData =[50 => 5,60=>4,100=>5];
+
+?>
