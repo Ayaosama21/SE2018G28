3596cb860e4af9784ab260127cbcf902fd61f60d
AsmaaSobhyy <36643080+AsmaaSobhyy@users.noreply.github.com>
2018-11-04 16:44:54 +0200

    Add files via upload

---
+++ assignment-1-testing/students.php
@@ -0,0 +1,74 @@
+<?php include("header.php"); ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>students</title>
+    
+</head>
+<body>
+<div class="container">
+<div  class="p-2 mt-3 text-align-center text-justify">
+		<span style="font-size: 150%;" class="text-monospace" >Students</span>
+		<button class=" float-right" id="0">Add Student</button>
+	</div>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Name</th>
+      <th scope="col"> </th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    	<td>1</td>
+    	<td>Ahmed Mohamed</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>2</td>
+    	<td>Ahmed Mohamed</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>3</td>
+    	<td>Ahmed Mohamed</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>4</td>
+    	<td>Ahmed Mohamed</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+    <tr>
+    	<td>5</td>
+    	<td>Ahmed Mohamed</td>
+    	<td>
+		<button class="button edit_student" id="1">Edit</button>&nbsp;
+		<button class="button delete_student" id="1">Delete</button>
+	    </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<footer class="footer">
+<?php include("footer.php"); ?>
+</footer>
+    
+</body>
+</html>
